d98eccc5988758f881678967d0078fa347194710
Nick González <nikol.gonzalez@pconnection.net>
2020-12-03 19:19:10 -0600

    images

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export default function NotFound() {
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { save, load } from "redux-localstorage-simple";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { Link } from "gatsby";
+
+import rootReducer from "../redux/reducers/rootReducer";
+
+import MarDeUranoApp from "../components/MarDeUranoApp";
+import ShopLayout from "../components/layouts/ShopLayout";
+
+const NotFound = ({ data, location }) => {
+  let store;
+
+  if (typeof window !== `undefined`) {
+    store = createStore(
+      rootReducer,
+      load(),
+      composeWithDevTools(applyMiddleware(thunk, save()))
+    );
+  } else {
+    store = createStore(
+      rootReducer,
+      composeWithDevTools(applyMiddleware(thunk))
+    );
+  }
+
   return (
-    <div>
-      <h1>Page Not Found</h1>
-      <p>Oops, we couldn't find this page!</p>
-    </div>
+    <Provider store={store}>
+      <MarDeUranoApp>
+        <ShopLayout headerTop="visible">
+          <div className="shop-area pt-95 pb-100">
+            <div className="container d-flex justify-content-center flex-column align-items-center">
+              <h2>Page not found</h2>
+              <h4>The page you are requesting does not exist</h4>
+              <Link className="btn btn-warning text-white" to="/shop">
+                Back to shop
+              </Link>
+            </div>
+          </div>
+        </ShopLayout>
+      </MarDeUranoApp>
+    </Provider>
   );
-}
+};
+
+export default NotFound;
