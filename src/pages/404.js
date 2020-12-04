00d9f32f5e484f6f508f7c0f3bef94a2e314d5c7
Nick González <nikol.gonzalez@pconnection.net>
2020-12-03 19:31:23 -0600

    images

--- src/pages/404.js
+++ src/pages/404.js
@@ -32,13 +32,25 @@ const NotFound = ({ data, location }) => {
     <Provider store={store}>
       <MarDeUranoApp>
         <ShopLayout headerTop="visible">
-          <div className="shop-area pt-95 pb-100">
-            <div className="container d-flex justify-content-center flex-column align-items-center">
-              <h2>Page not found</h2>
-              <h4>The page you are requesting does not exist</h4>
-              <Link className="btn btn-warning text-white" to="/shop">
-                Back to shop
-              </Link>
+          <div className="error-area pt-40 pb-100">
+            <div className="container">
+              <div className="row justify-content-center">
+                <div className="col-xl-7 col-lg-8 text-center">
+                  <div className="error">
+                    <h1>404</h1>
+                    <h2>OPPS! PAGE NOT FOUND</h2>
+                    <p>
+                      Sorry but the page you are looking for does not exist,
+                      have been removed, name changed or is temporarity
+                      unavailable.
+                    </p>
+
+                    <Link to={"/shop"} className="error-btn">
+                      Back to shop page
+                    </Link>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </ShopLayout>
